29ceee0953a9428ea7ad86368aeb8611ba78b12a
Prajjwal Pachauri <prajjwalpachauri725@gmail.com>
2025-07-25 16:21:20 +0530

    Created DatabaseConnection.java class

---
+++ src/main/java/database/DatabaseConnection.java
@@ -0,0 +1,14 @@
+/* Created by IntelliJ IDEA.
+
+Author: Prajjwal Pachauri(cypher)
+Date: 25-07-2025
+Time: 4:08 PM
+File: DatabaseConnection.java */
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+}
