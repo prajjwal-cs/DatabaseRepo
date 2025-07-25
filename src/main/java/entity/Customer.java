98ed8bbe71aa4bbb8c2eb4b53308f76b995b1331
Prajjwal Pachauri <prajjwalpachauri725@gmail.com>
2025-07-25 12:00:12 +0530

    Created 2 Entities customer and device

---
+++ src/main/java/entity/Customer.java
@@ -0,0 +1,11 @@
+/* Created by IntelliJ IDEA.
+
+Author: Prajjwal Pachauri(cypher)
+Date: 25-07-2025
+Time: 11:51 AM
+File: Customer.java */
+package entity;
+
+public class Customer {
+
+}
---
+++ src/main/java/entity/Device.java
@@ -0,0 +1,10 @@
+/* Created by IntelliJ IDEA.
+
+Author: Prajjwal Pachauri(cypher)
+Date: 25-07-2025
+Time: 11:50 AM
+File: Device.java */
+package entity;
+
+public class Device {
+}
