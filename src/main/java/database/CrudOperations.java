ae5f0bc795765fe49a6d4fb05121ed0bcab21f03
Prajjwal Pachauri <prajjwalpachauri725@gmail.com>
2025-07-25 16:23:31 +0530

    Created CrudOperations.java class

---
+++ src/main/java/database/CrudOperations.java
@@ -0,0 +1,11 @@
+/* Created by IntelliJ IDEA.
+
+Author: Prajjwal Pachauri(cypher)
+Date: 25-07-2025
+Time: 4:22 PM
+File: DatabaseOperations.java */
+package database;
+
+public class CrudOperations {
+
+}
