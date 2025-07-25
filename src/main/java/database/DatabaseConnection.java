4c048eefac72ecfd44f68243a7bed72b8edc6880
Prajjwal Pachauri <prajjwalpachauri725@gmail.com>
2025-07-25 16:29:18 +0530

    update DatabaseConnection.java

--- src/main/java/database/DatabaseConnection.java
+++ src/main/java/database/DatabaseConnection.java
@@ -14,8 +14,19 @@ public class DatabaseConnection {
     private final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private final String USERNAME = "postgres";
     private final String PASSWORD = "123";
+    private static Connection connection;
 
-    public Connection createConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    public Connection createConnection()  {
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            if (connection != null) {
+                System.out.println("Connected to database successfully");
+            } else {
+                System.out.println("Couldn't connect to database");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return connection;
     }
 }
