b2d57cd20a494f2c7dab7bd6e251f38f11cb319f
Prajjwal Pachauri <prajjwalpachauri725@gmail.com>
2025-07-28 11:29:31 +0530

    update3

--- src/main/java/main/DeviceMain.java
+++ src/main/java/main/DeviceMain.java
@@ -19,45 +19,46 @@ public class DeviceMain {
         CrudOperations<Device> deviceCrudOperations =  new DeviceCRUD(connection.createConnection());
         Device device = new Device();
         Scanner scanner = new Scanner(System.in);
-        int choice;
-        System.out.println("Welcome to Device Management System");
-        System.out.println("1. Create Device");
-        System.out.println("2. Read Device");
-        System.out.println("3. Update Device");
-        System.out.println("4. Delete Device");
-        System.out.println("5. Exit");
-        choice = scanner.nextInt();
+        int choice = 0;
         try {
-            switch (choice) {
-                 case 1: {
-                     System.out.println("Enter device name, type, manufacturer_name");
-                     device.setName(scanner.next());
-                     device.setType(scanner.next());
-                     device.setManufacturer(scanner.next());
-                     deviceCrudOperations.create(device);
-                     break;
-                 }
-                 case 2: {
-                     System.out.print("Enter device id to read: ");
-                     deviceCrudOperations.read(scanner.nextInt());
-                     break;
-                 }
-                 case 3: {
-                     System.out.print("Enter device id to update: ");
-                     int id = scanner.nextInt();
-                     System.out.println("Enter device name, type, manufacturer: ");
-                     device.setName(scanner.next());
-                     device.setType(scanner.next());
-                     device.setManufacturer(scanner.next());
-                     deviceCrudOperations.update(id, device);
-                     break;
-                 }
-                 case 4: {
-                     System.out.print("Enter device id to delete: ");
-                     int id = scanner.nextInt();
-                     deviceCrudOperations.delete(id);
-                 }
-
+            while(choice != 5) {
+                System.out.println("Welcome to Device Management System");
+                System.out.println("1. Create Device");
+                System.out.println("2. Read Device");
+                System.out.println("3. Update Device");
+                System.out.println("4. Delete Device");
+                System.out.println("5. Exit");
+                choice = scanner.nextInt();
+                switch (choice) {
+                    case 1: {
+                        System.out.println("Enter device name, type, manufacturer_name");
+                        device.setName(scanner.next());
+                        device.setType(scanner.next());
+                        device.setManufacturer(scanner.next());
+                        deviceCrudOperations.create(device);
+                        break;
+                    }
+                    case 2: {
+                        System.out.print("Enter device id to read: ");
+                        deviceCrudOperations.read(scanner.nextInt());
+                        break;
+                    }
+                    case 3: {
+                        System.out.print("Enter device id to update: ");
+                        int id = scanner.nextInt();
+                        System.out.println("Enter device name, type, manufacturer: ");
+                        device.setName(scanner.next());
+                        device.setType(scanner.next());
+                        device.setManufacturer(scanner.next());
+                        deviceCrudOperations.update(id, device);
+                        break;
+                    }
+                    case 4: {
+                        System.out.print("Enter device id to delete: ");
+                        int id = scanner.nextInt();
+                        deviceCrudOperations.delete(id);
+                    }
+                }
             }
         } catch (Exception e) {
             System.out.println("Invalid input Please try again...");
